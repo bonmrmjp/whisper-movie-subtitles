013f97a66b6c13ae799251db504d03cfd14cfb3b
bonmrmjp <132231226+bonmrmjp@users.noreply.github.com>
2023-04-30 23:10:21 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name='whisper-movie-subtitles',
     version='1.0',
-#    py_modules=['whisper-movie-subtitles'],    
+    py_modules=['whisper-movie-subtitles'],    
     entry_points={
         'console_scripts': [
-            'whisper-movie-subtitles=whisper_movie_subtitles:main',
+            'whisper-movie-subtitles=whisper-movie-subtitles:main',
         ],
     },
     install_requires=[
